f360aa3457bbd5691b5c38fb0535bc6755fd7b1c
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-03-12 04:19:11 +0530

    taking back the changes

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    extend:{
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-    }
   },
   images: {
     domains: ["dummyimage.com", "mf.b37mrtl.ru", "cdnn1.img.sputniknews.com"],
