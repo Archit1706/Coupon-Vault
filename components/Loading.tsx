954b7c11c4a73a8bb3cd0baf0949abc931dd75e2
Archit <architrathod77@gmail.com>
2023-03-12 04:20:04 +0530

    Loaders added

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -4,7 +4,13 @@ import React from "react";
 import ReactLoading from "react-loading";
 
 const LoadingComponent = ({ type, color }) => (
-    <ReactLoading type={type} color={color} height={667} width={375} />
+    <ReactLoading
+        className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        type={type}
+        color={color}
+        height={80}
+        width={160}
+    />
 );
 
 export default LoadingComponent;
--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,17 +1,23 @@
-import CouponCard from 'components/CouponCard'
-import React from 'react'
+import CouponCard from "components/CouponCard";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Docs = (props: Props) => {
-  return (
-    <div className='p-16 space-y-5' >
-        <CouponCard />
-        <CouponCard />
-        <CouponCard />
-        <CouponCard />
-    </div>
-  )
-}
+    return <div className="p-16 space-y-5"></div>;
+};
 
-export default Docs
+// export default Docs
+
+// "use client";
+// import LoadingComponent from "components/Loading";
+
+// export default function Loading() {
+//     return (
+//         <>
+//             <div>
+//                 <LoadingComponent type={"cubes"} color={"#0984e3"} />
+//             </div>
+//         </>
+//     );
+// }
