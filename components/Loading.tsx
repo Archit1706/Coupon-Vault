6ea26f3e6563e0a7dac8492015b399570b38f558
Archit <architrathod77@gmail.com>
2023-03-12 04:04:13 +0530

    some msg

---
+++ components/Loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import ReactLoading from "react-loading";
+
+const LoadingComponent = ({ type, color }) => (
+    <ReactLoading type={type} color={color} height={667} width={375} />
+);
+
+export default LoadingComponent;
